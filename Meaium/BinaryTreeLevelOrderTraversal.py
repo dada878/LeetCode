2a604d98ff6192f53af7301a8807af1006d6ae1d
Dada878 <xx0932399@gmail.com>
2022-10-16 00:50:18 +0800

    第四題Medium

---
+++ Meaium/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,25 @@
+"Link: https://leetcode.com/problems/binary-tree-level-order-traversal/"
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+def traversal(node, depth=0, result=[]):
+    print(result)
+    if node == None:
+        return
+    if len(result) < depth+1:
+        result.append([])
+    traversal(node.left, depth=depth+1, result=result)
+    traversal(node.right, depth=depth+1, result=result)
+    result[depth].append(node.val)
+    print(result)
+    return result
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        return traversal(root, result=[])
