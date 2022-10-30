f1c8101347fafb7f0f87b79315da0b7e6b9311a8
Dada878 <xx0932399@gmail.com>
2022-10-30 12:08:21 +0800

    two points cpp

---
+++ Easy/MergeSortedArray.cpp
@@ -0,0 +1,27 @@
+// Link: https://leetcode.com/problems/merge-sorted-array
+// Thanks: https://www.youtube.com/watch?v=P1Ic85RarKY
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int last = m + n -1;
+        while (m > 0 && n > 0) {
+            if (nums1[m-1] > nums2[n-1]) {
+                nums1[last] = nums1[m-1];
+                m--;
+            } else {
+                nums1[last] = nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+        while (n > 0) {
+            nums1[last] = nums2[n - 1];
+            n--;
+            last--;
+        }
+    }
+};
