7cea66a29cd404077f904eec1d282514e53b7324
冰川 <xx0932399@gmail.com>
2022-11-07 13:10:02 +0800

    好冗長 :((

---
+++ Easy/Maximum69Number.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int numLen(int num)
+    {
+        int result = 0;
+        while (num > 1)
+        {
+            num /= 10;
+            result++;
+        }
+        return result;
+    }
+    int *numToArray(int num)
+    {
+        int len = numLen(num);
+        int *result = new int[5];
+        for (int i = len - 1; i >= 0; i--)
+        {
+            result[i] = num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+    int maximum69Number(int num)
+    {
+        int len = numLen(num);
+        int *nums = numToArray(num);
+        for (int i = 0; i < len; i++)
+        {
+            if (nums[i] == 6)
+            {
+                nums[i] = 9;
+                break;
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < len; i++)
+        {
+            result += nums[i] * pow(10, len - i - 1);
+        }
+        return result;
+    }
+};
