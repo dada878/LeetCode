1edec1c1160962f8c69a93442e4ebfa46184d45d
冰川 <xx0932399@gmail.com>
2022-11-19 15:49:36 +0800

    it's not my code :((

---
+++ Hard/ErectTheFence.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/erect-the-fence
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <set>
+using namespace std;
+class Solution 
+{
+public:
+    
+    using tree = vector<int>;
+    
+    vector<tree> outerTrees(vector<tree>& trees) 
+    {
+        auto cross = [](tree& B, tree& A, tree& T) -> int
+        {
+            return (T[1]-B[1])*(B[0]-A[0]) - (B[1]-A[1])*(T[0]-B[0]);
+        };
+        
+        sort(trees.begin(), trees.end());
+        
+        deque<tree> F;
+        
+        for (tree T : trees)
+        {
+            while (F.size() >= 2 && cross(F[F.size()-1],F[F.size()-2],T) < 0)
+                F.pop_back();
+            F.push_back(T);
+
+            while (F.size() >= 2 && cross(F[0],F[1],T) > 0)
+                F.pop_front();
+            F.push_front(T);
+        }
+        
+        set<tree> unique(F.begin(), F.end());
+        return vector<tree>(unique.begin(), unique.end());
+    }
+};
