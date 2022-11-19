0e93ff4c734409d4d52fe4d95465feb973065071
冰川 <xx0932399@gmail.com>
2022-11-19 14:19:28 +0800

    first dp problem full solutuin by myself

---
+++ Medium/JumpGame.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/jump-game
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if (nums.size() == 1) return true;
+        vector<int> dp(nums.size()-1);
+        dp[0] = 0;
+        for (int i = 0; i < nums.size()-1; i++) {
+            dp[i] = max(dp[max(i-1, 0)]-1, nums[i]);
+            if (dp[i] == 0) return false;
+        }
+        return dp[dp.size()-1] > 0 ? true : false;
+    }
+};
