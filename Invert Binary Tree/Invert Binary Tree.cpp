14cc5d7295e4409a0a7f17b0fb32fddd816517ce
冰川 <37009584+dada878@users.noreply.github.com>
2022-11-28 01:28:27 +0800

    Time: [object HTMLSpanElement], Memory: [object HTMLSpanElement] - LeetHub

--- Invert Binary Tree/Invert Binary Tree.cpp
+++ Invert Binary Tree/Invert Binary Tree.cpp
@@ -1,23 +1,12 @@
 class Solution {
-
 public:
-
     TreeNode* invertTree(TreeNode* root) {
-
         if(!root) return NULL;
-
         else{     
-
             swap(root-&gt;left,root-&gt;right);
-
             invertTree(root-&gt;left);
-
             invertTree(root-&gt;right);
-
         }
-
         return root;
-
     }
-
 };
