2010bcdda543a5c66d123e82e76236eb8a6e45c9
冰川 <37009584+dada878@users.noreply.github.com>
2022-12-06 23:53:20 +0800

    Time: 92 ms, Memory: 16.7 MB - LeetHub

---
+++ Odd-Even-Linked-List/Odd-Even-Linked-List.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        """
+        if head is None:
+            return head
+        currentOdd = head
+        count = 2
+        node = head
+        pre = node
+        node = node.next
+        while node is not None:
+            if count % 2 == 0:
+                pre = node
+                node = node.next
+            else:
+                next = node.next
+                pre.next = next
+                temp = currentOdd.next
+                currentOdd.next = node
+                node.next = temp
+                currentOdd = node
+                node = next
+            count += 1
+        return head
