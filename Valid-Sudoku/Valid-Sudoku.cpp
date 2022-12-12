d91db4ece11b9c3ce03177b2957a935a95cd3534
冰川 <37009584+dada878@users.noreply.github.com>
2022-12-12 23:06:49 +0800

    Time: 31 ms, Memory: 19.9 MB - LeetHub

--- Valid-Sudoku/Valid-Sudoku.cpp
+++ Valid-Sudoku/Valid-Sudoku.cpp
@@ -3,16 +3,35 @@ class Solution
 public:
     bool isValidSudoku(vector&lt;vector&lt;char&gt;&gt;&amp; board) 
     {
-        vector&lt;set&lt;char&gt;&gt; rows(9), cols(9), blck(9);                // memoize seen digits
-        for (int i = 0; i &lt; 9; i++) 
-            for (int j = 0; j &lt; 9; j++) 
-                if (board[i][j] != '.')
-                {
-                    int d = board[i][j], b = (i/3)*3+j/3;
-                    if (!rows[i].emplace(d).second) return false;   // [1] test rows
-                    if (!cols[j].emplace(d).second) return false;   // [2] test columns
-                    if (!blck[b].emplace(d).second) return false;   // [3] test blocks
+        // check row
+        for (int i = 0; i &lt; 9; i++) {
+            map&lt;char, int&gt; table;
+            for (int j = 0; j &lt; 9; j++) {
+                if (board[i][j] != '.' &amp;&amp; table[board[i][j]] &gt; 0) return false;
+                table[board[i][j]]++;
+            }
+        }
+        // check column
+        for (int i = 0; i &lt; 9; i++) {
+            map&lt;char, int&gt; table;
+            for (int j = 0; j &lt; 9; j++) {
+                if (board[j][i] != '.' &amp;&amp; table[board[j][i]] &gt; 0) return false;
+                table[board[j][i]]++;
+            }
+        }
+        // check grid area
+        for (int i = 0; i &lt; 3; i++) {
+            for (int j = 0; j &lt; 3; j++) {
+                map&lt;char, int&gt; table;
+                for (int k = i * 3; k &lt; (i+1) * 3; k++) {
+                    for (int l = j * 3; l &lt; (j+1) * 3; l++) {
+                        cout &lt;&lt; k &lt;&lt; l &lt;&lt; endl;
+                        if (board[k][l] != '.' &amp;&amp; table[board[k][l]] &gt; 0) return false;
+                        table[board[k][l]]++;
+                    }
                 }
+            }
+        }
         return true;
     }
 };
