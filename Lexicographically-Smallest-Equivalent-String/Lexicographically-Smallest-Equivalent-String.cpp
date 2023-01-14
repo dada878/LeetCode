f763b6c1e0bd8493e70d28a0a4944cba8cf48102
冰川 <37009584+dada878@users.noreply.github.com>
2023-01-14 22:49:55 +0800

    Time: 7 ms, Memory: 7.6 MB - LeetHub

--- Lexicographically-Smallest-Equivalent-String/Lexicographically-Smallest-Equivalent-String.cpp
+++ Lexicographically-Smallest-Equivalent-String/Lexicographically-Smallest-Equivalent-String.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    int charToInt(char chr) {
-        return chr - 'a';
-    }
-    char intToChar(int num) {
-        return num + 'a';
-    }
     int dfs(vector&lt;vector&lt;int&gt;&gt;&amp; graph, int vertex, vector&lt;bool&gt;&amp; visited) {
         int result = vertex;
         for (auto edge : graph[vertex]) {
@@ -19,19 +13,19 @@ public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         vector&lt;vector&lt;int&gt;&gt; graph(26);
         for (int i = 0; i &lt; s1.length(); i++) {
-            graph[charToInt(s1[i])].push_back(charToInt(s2[i]));
-            graph[charToInt(s2[i])].push_back(charToInt(s1[i]));
+            graph[s1[i] - 'a'].push_back(s2[i] - 'a');
+            graph[s2[i] - 'a'].push_back(s1[i] - 'a');
         }
         map&lt;char, char&gt; dp;
         vector&lt;bool&gt; visited(26);
         string result;
         for (auto chr : baseStr) {
             fill(visited.begin(), visited.end(), false);
-            int start = charToInt(chr);
+            int start = chr - 'a';
             visited[start] = true;
             if (dp.find(chr) != dp.end()) result.push_back(dp[chr]);
             else {
-                char tmp = intToChar(dfs(graph, start, visited));
+                char tmp = dfs(graph, start, visited) + 'a';
                 dp[chr] = tmp;
                 result.push_back(tmp);
             }
