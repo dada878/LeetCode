dffab61c483d15bea344e772566a77eecdd1fdd2
冰川 <37009584+dada878@users.noreply.github.com>
2023-02-09 05:30:16 +0800

    Time: 594 ms, Memory: 17.3 MB - LeetHub

--- Jump-Game-II/Jump-Game-II.cpp
+++ Jump-Game-II/Jump-Game-II.cpp
@@ -1,35 +1,13 @@
-// class Solution {
-// public:
-//     int solve(vector&lt;int&gt; nums, int start) {
-//         int step = nums[start];
-//         int maxStep = 0;
-//         for (int i = start + 1; i &lt;= step + start; i++) {
-//             for (int k = start + 1; k &lt; start + nums[i] - i + 1; k++) {
-//                 maxStep = max(i + nums[k], maxStep);
-//             }
-//         }
-//         return maxStep;
-//     }
-//     int jump(vector&lt;int&gt;&amp; nums) {
-//         cout &lt;&lt; solve(nums, 0) &lt;&lt; endl;
-//         cout &lt;&lt; solve(nums, nums[0]) &lt;&lt; endl;
-//         return 1;
-//     }
-// };
 class Solution {
 public:
     int jump(vector&lt;int&gt;&amp; nums) {
-      for(int i = 1; i &lt; nums.size(); i++)
-      {
-        nums[i] = max(nums[i] + i, nums[i-1]);
-      }
-      int ind = 0;
-      int ans = 0;
-      while(ind &lt; nums.size() - 1)
-      {
-        ans++;
-        ind = nums[ind];
-      }
-      return ans;
+        vector&lt;int&gt; dp(nums.size(), INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i &lt; nums.size(); i++) {
+            for (int j = 1; j &lt;= nums[i] &amp;&amp; i + j &lt; nums.size(); j++) {
+                dp[i + j] = min(dp[i + j], dp[i] + 1);
+            }
+        }
+        return dp[nums.size()-1];
     }
 };
