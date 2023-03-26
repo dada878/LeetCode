9d9ae72cabbc3acd6ffa4bb6993da6a6e2a3f42c
Dada878 <37009584+dada878@users.noreply.github.com>
2023-03-26 18:43:29 +0800

    Time: 0 ms, Memory: 10 MB - LeetHub

--- Final-Prices-With-a-Special-Discount-in-a-Shop/Final-Prices-With-a-Special-Discount-in-a-Shop.cpp
+++ Final-Prices-With-a-Special-Discount-in-a-Shop/Final-Prices-With-a-Special-Discount-in-a-Shop.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     vector&lt;int&gt; finalPrices(vector&lt;int&gt;&amp; prices) {
         stack&lt;int&gt; s;
-        vector&lt;int&gt; result(prices.size());
-        prices.push_back(0);
+        vector&lt;int&gt; result(prices.size(), -1);
         for (int i = 0; i &lt; prices.size(); i++) {
+            // cout &lt;&lt; "===== " &lt;&lt; prices[i] &lt;&lt;  " =====" &lt;&lt; endl;
             while (!s.empty() &amp;&amp; prices[s.top()] &gt;= prices[i]) {
+                // cout &lt;&lt; "pop : " &lt;&lt; prices[s.top()] &lt;&lt; endl;
+                // cout &lt;&lt; "val : " &lt;&lt; prices[s.top()] - prices[i] &lt;&lt; endl;
                 result[s.top()] = prices[s.top()] - prices[i];
                 s.pop();
             }
             s.push(i);
+        }
+        for (int i = 0; i &lt; prices.size(); i++) {
+            if (result[i] != -1) continue;
+            result[i] = prices[i];
         }
         return result;
     }
