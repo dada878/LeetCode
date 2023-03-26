329ec41be893ff4b5a38fafae6c4a8caf48690f1
Dada878 <37009584+dada878@users.noreply.github.com>
2023-03-26 16:27:34 +0800

    Time: 607 ms, Memory: 226.9 MB - LeetHub

--- Longest-Cycle-in-a-Graph/Longest-Cycle-in-a-Graph.cpp
+++ Longest-Cycle-in-a-Graph/Longest-Cycle-in-a-Graph.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int ans = -1;
     void dfs(vector&lt;int&gt;&amp; edges, unordered_map&lt;int, int&gt;&amp; dist, vector&lt;bool&gt;&amp; visited, int vertex, int step) {
-        // cout &lt;&lt; "visit : " &lt;&lt; vertex &lt;&lt; endl;
         visited[vertex] = true;
         dist[vertex] = step;
         int next = edges[vertex];
@@ -16,7 +15,6 @@ public:
         int n = edges.size();
         vector&lt;bool&gt; visited(n);
         for (int i = 0; i &lt; n; i++) {
-            // cout &lt;&lt; "check visit " &lt;&lt; i &lt;&lt; " : " &lt;&lt; visited[i] &lt;&lt; endl;
             if (visited[i]) continue;
             unordered_map&lt;int, int&gt; dist;
             dfs(edges, dist, visited, i, 1);
@@ -24,30 +22,3 @@ public:
         return ans;
     }
 };
-// class Solution {
-// public:
-//     int answer = -1;
-//     void dfs(int node, vector&lt;int&gt;&amp; edges, unordered_map&lt;int, int&gt;&amp; dist, vector&lt;bool&gt;&amp; visit) {
-//         cout &lt;&lt; "visit : " &lt;&lt; node &lt;&lt; endl;
-//         visit[node] = true;
-//         int neighbor = edges[node];
-//         if (neighbor != -1 &amp;&amp; !visit[neighbor]) {
-//             dist[neighbor] = dist[node] + 1;
-//             dfs(neighbor, edges, dist, visit);
-//         } else if (neighbor != -1 &amp;&amp; dist.count(neighbor)) {
-//             answer = max(answer, dist[node] - dist[neighbor] + 1);
-//         }
-//     }
-//     int longestCycle(vector&lt;int&gt;&amp; edges) {
-//         int n = edges.size();
-//         vector&lt;bool&gt; visit(n);
-//         for (int i = 0; i &lt; n; i++) {
-//             if (!visit[i]) {
-//                 unordered_map&lt;int, int&gt; dist;
-//                 dist[i] = 1;
-//                 dfs(i, edges, dist, visit);
-//             }
-//         }
-//         return answer;
-//     }
-// };
