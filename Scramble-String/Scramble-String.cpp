6158c2aaf90d46c58db2f0727ca2459b87219cdd
Dada878 <37009584+dada878@users.noreply.github.com>
2023-03-30 23:53:14 +0800

    Time: 174 ms, Memory: 36.4 MB - LeetHub

---
+++ Scramble-String/Scramble-String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map&lt;string,bool&gt; mp;
+    bool isScramble(string s1, string s2) {
+        int n = s1.size();
+        if(s2.size()!=n) return false;
+        if(s1==s2) return true;
+        if(n==1) return false;
+        string key = s1+" "+s2;
+        if(mp.find(key)!=mp.end()) return mp[key];
+        for(int i=1;i&lt;n;i++) {
+            bool withoutswap =(isScramble(s1.substr(0,i),s2.substr(0,i)) &amp;&amp; isScramble(s1.substr(i),s2.substr(i)));
+            if(withoutswap) return true;
+            bool withswap=(isScramble(s1.substr(0,i),s2.substr(n-i)) &amp;&amp; isScramble(s1.substr(i),s2.substr(0,n-i)));
+            if(withswap) return true;
+        }
+        return mp[key] = false;
+    }
+};
